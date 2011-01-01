d56a2527c01403a155bd374127d81faceafc6343
Mike D Pilsbury <mike.pilsbury@gmail.com>
2011-01-01 12:54:20 +0000

    Remove unused client side javascript; all the socket.io stuff.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,38 +1,4 @@
-require(['urlparser', 'socket.io/socket.io.js'], function(urlParser) {
+require([], function() {
   $(function() {
-    var user = urlParser.getQueryParameter('user');
-    console.log('user: ' + user);
-    
-    var socket = new io.Socket(); 
-    socket.on('connect', function(){
-      console.log('connect');
-      socket.send(JSON.stringify({
-        method: 'hello', 
-        user: user}));
-    });
-    
-    socket.on('message', function(message) {
-      if (message.method === 'clients') {
-        
-        var clients = '';
-        for (var c = 0; c < message.clients.length; c++) {
-          clients += message.clients[c] + '\r\n';
-        }
-        
-        $('.user-count').text(message.clients.length);
-        $('.users').text(clients);
-      }
-
-      if (message.method === 'status') {
-        $('.status').text(message.status);
-        $('.countdown').text(message.remaining);
-      }
-    }); 
-
-    socket.on('disconnect', function(){ 
-      console.log('disconnect');
-    });
-    
-    socket.connect();
   });
 });
